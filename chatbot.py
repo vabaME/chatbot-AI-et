c2ebfa6604ce5d29d1d3943eae5ba6a5aa97f896
vaba <92581921+vabaME@users.noreply.github.com>
2023-03-24 20:10:59 +0200

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,38 @@
+import os
+import openai
+
+openai.api_key="SINU API VÕTI"
+messages = []
+
+# leia olemasolevad vestlusfailid
+files = [f for f in os.listdir() if f.startswith("vestlus-") and f.endswith(".txt")]
+# loo uus failinimi
+if files:
+    last_file = max(files)
+    last_num = int(last_file.split("-")[1].split(".")[0])
+    new_num = last_num + 1
+else:
+    new_num = 1
+filename = f"vestlus-{new_num}.txt"
+
+print("Sa võid nüüd vestelda.")
+with open(filename, "w", encoding="utf-8") as file:
+    while True:
+        message = input("")
+        messages.append({"role":"user","content": message})
+        file.write("User: " + message + "\n")
+
+        response=openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=messages
+        )
+
+        reply = response["choices"][0]["message"]["content"]
+        print(reply)
+        file.write("Bot: " + reply + "\n")
+
+        if "goodbye" in message.lower():
+            break
+
+    file.close()
+
---
+++ readme.md
@@ -0,0 +1,12 @@
+Python koodis OpenAI API, mis salvestab sinu vestluse vestlus-NUMBER.txt faili.
+Ja programm lõpetab töö ja salvestab kõik vestluse kui sisestad sõna "goodbye".
+
+Ja sul peab olema ka oma openai VÕTI (KEY), mille pead sa lisama skripti vastavale võtmeväljale.
+Kasutab siis gpt-3.5-turbo mudelit.
+
+Kasutamine terminalis:
+$python3 chatbot.py
+
+Kui tööle ei lähe, siis on puudu -
+import openai jaoks peab eelnevalt paigaldama: 
+pip install openai
